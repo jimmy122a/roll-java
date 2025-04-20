ddbec043b3d8c56ccf027c58ef0d3afe08f988bf
jimmy122a <ceejayjimmy12@gmail.com>
2025-04-20 11:18:46 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const mybutton = document.getElementById("mybutton")
+const mylabel = document.getElementById("mylabel")
+const min = 1;
+const max = 6;
+let randomNum;
+
+mybutton.onclick = function(){
+    randomNum = Math.floor(Math.random()*max) +min;
+    mylabel.textContent = randomNum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
